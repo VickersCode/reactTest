bcb1ad079a0b85573302baefea5a68f8300fb0ff
Tyler Vickers <67883080+VickersCode@users.noreply.github.com>
2023-06-20 14:23:06 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,12 @@
+const navbar = (
+    <nav>
+        <h1>React Page</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contacts</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
